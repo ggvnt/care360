deb143b7b22c72830c775125da29c5a776ca7e9a
Vishmitha Nipun <128218923+ggvnt@users.noreply.github.com>
2025-03-20 13:58:50 +0530

    symptom filter

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,9 @@ import authRoutes from "./routes/auth/auth.route.js";
 //admin
 import symptomRoutes from "./routes/admins/symptom.route.js";
 
+//vishmitha
+import symptomCheckerRouters from "./routes/vishmitha/symptomChecker.route.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -27,6 +30,8 @@ app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", symptomRoutes);
+
+app.use("/api/symptom-checker", symptomCheckerRouters);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
