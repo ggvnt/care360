eb6e5dfdcb12c4debdee46961e6649b115fc2d00
ggvnt <it22116354@my.sliit.lk>
2025-04-30 07:21:11 +0530

    refactor: remove symptom checker routes and controller; update dependencies

--- backend/src/routes/vishmitha/symptomChecker.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { checkSymptoms } from "../../contrallers/vishmitha/symptomChecker.controllers.js";
-
-const router = express.Router();
-
-//Route for Symptom Checker
-router.post("/symptoms/check", checkSymptoms);
-
-export default router;
